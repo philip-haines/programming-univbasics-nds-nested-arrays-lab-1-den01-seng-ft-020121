65eb0ac5b84b466c52689f5a39ae6dbb72fe50d4
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-23 21:31:18 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -51,7 +51,8 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
   
-  matrix_update[row][column] = new_value
+  matrix[row][column] = new_value
   binding.pry
+  prints "hello"
   
 end
